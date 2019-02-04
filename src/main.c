9b6aec92c5d40a676a0e2eba585bdc60d89162c7
coon <coon@mailbox.org>
2019-02-04 13:04:26 +0100

    refactor + implement uart print function

--- src/main.c
+++ src/main.c
@@ -8,10 +8,17 @@
 
 // Private function prototypes
 static void delayMs(uint32_t timeMs);
-static void init();
+static void initLed();
+static void initUart(uint32_t baudrate);
+static void initDelayTimer();
+static void print(const char* pText);
 
 int main(void) {
-  init();
+  initDelayTimer();
+  initUart(115200);
+  initLed();
+
+  print("Hello world!\n");
 
   while (true) {
     GPIO_SetBits(GPIOD, GPIO_Pin_12);
@@ -23,18 +30,15 @@ int main(void) {
   return 0;
 }
 
-void init() {
-  GPIO_InitTypeDef GPIO_InitStructure;
-  USART_InitTypeDef USART_InitStructure;
-
-  // ---------- SysTick timer --------
+static void initDelayTimer() {
   if (SysTick_Config(SystemCoreClock / 1000)) {
-    // Capture error
 	  while (true)
       ;
   }
+}
 
-  // ------ LED ------
+static void initLed() {
+  GPIO_InitTypeDef GPIO_InitStructure;
 
   RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
 
@@ -44,31 +48,58 @@ void init() {
   GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
   GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
   GPIO_Init(GPIOD, &GPIO_InitStructure);
+}
 
-  // ------ UART ------
+static void initUart(uint32_t baudrate) {
+  GPIO_InitTypeDef GPIO_InitStruct;
+  USART_InitTypeDef USART_InitStruct;
+  NVIC_InitTypeDef NVIC_InitStructure;
 
-  RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART2, ENABLE);
-  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
+  RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1, ENABLE);
+  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOB, ENABLE);
 
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_2 | GPIO_Pin_3;
-  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
-  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
-  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
-  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;
-  GPIO_Init(GPIOA, &GPIO_InitStructure);
+  GPIO_InitStruct.GPIO_Pin = GPIO_Pin_6 | GPIO_Pin_7;
+  GPIO_InitStruct.GPIO_Mode = GPIO_Mode_AF;
+  GPIO_InitStruct.GPIO_Speed = GPIO_Speed_100MHz;
+  GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
+  GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_UP;
+  GPIO_Init(GPIOB, &GPIO_InitStruct);
 
-  GPIO_PinAFConfig(GPIOA, GPIO_PinSource2, GPIO_AF_USART2);
-  GPIO_PinAFConfig(GPIOA, GPIO_PinSource3, GPIO_AF_USART2);
+  GPIO_PinAFConfig(GPIOB, GPIO_PinSource6, GPIO_AF_USART1);
+  GPIO_PinAFConfig(GPIOB, GPIO_PinSource7, GPIO_AF_USART1);
 
-  USART_InitStructure.USART_BaudRate = 115200;
-  USART_InitStructure.USART_WordLength = USART_WordLength_8b;
-  USART_InitStructure.USART_StopBits = USART_StopBits_1;
-  USART_InitStructure.USART_Parity = USART_Parity_No;
-  USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
-  USART_InitStructure.USART_Mode = USART_Mode_Tx | USART_Mode_Rx;
-  USART_Init(USART2, &USART_InitStructure);
+  USART_InitStruct.USART_BaudRate = baudrate;
+  USART_InitStruct.USART_WordLength = USART_WordLength_8b;
+  USART_InitStruct.USART_StopBits = USART_StopBits_1;
+  USART_InitStruct.USART_Parity = USART_Parity_No;
+  USART_InitStruct.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+  USART_InitStruct.USART_Mode = USART_Mode_Tx | USART_Mode_Rx;
+  USART_Init(USART1, &USART_InitStruct);
 
-  USART_Cmd(USART2, ENABLE);
+  USART_ITConfig(USART1, USART_IT_RXNE, ENABLE);
+  NVIC_InitStructure.NVIC_IRQChannel = USART1_IRQn;
+  NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
+  NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
+  NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+  NVIC_Init(&NVIC_InitStructure);
+
+  USART_Cmd(USART1, ENABLE);
+
+  while (USART_GetFlagStatus(USART1, USART_FLAG_TC) == RESET)
+    ;
+}
+
+static void printChar(char c) {
+  // Wait until previous transmission is finished:
+  while (USART_GetFlagStatus(USART1, USART_FLAG_TXE) == RESET)
+    ;
+
+  USART_SendData(USART1, (u8) c);
+}
+
+static void print(const char* pText) {
+  for (; *pText; ++pText)
+    printChar(*pText);
 }
 
 volatile uint32_t _timeVar1;
